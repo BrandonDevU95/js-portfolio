68ef5b1be15a6625da58efbe248d6379e7f8b1d5
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-03-20 13:38:10 -0600

    Webpack Bundle Analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin =
+	require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
 	//Este es el punto de entrada de la aplicación
@@ -107,6 +109,7 @@ module.exports = {
 			],
 		}),
 		new Dotenv(),
+		new BundleAnalyzerPlugin(),
 	],
 	devServer: {
 		static: path.join(__dirname, 'dist'),
